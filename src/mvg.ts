ee40668acd8799616684ff6ad1cd9304982aa365
matkrin <matkrin@protonmail.com>
2022-08-17 07:18:10 +0200

    Pulled subcommands out of main command

--- src/mvg.ts
+++ src/mvg.ts
@@ -1,18 +1,27 @@
 import { Command } from "cliffy/command/mod.ts";
 import { renderDepartures, renderNotifications, renderRoutes } from "./cli.ts";
 
+const notifications = new Command()
+    .description("Show Notifications")
+    .action(async () => await renderNotifications());
+
+const departures = new Command()
+    .description("Show Departures")
+    .arguments("<stationName>")
+    .action(async (_, stationName) => await renderDepartures(stationName));
+
+const routes = new Command()
+    .description("Show Routes")
+    .arguments("<fromStation> <toStation>")
+    .action(async (_, fromStation, toStation) =>
+        await renderRoutes(fromStation, toStation)
+    );
+
 new Command()
     .name("mvg")
     .version("0.1.0")
     .description("Command line interface for MVG services")
-    .command("n notifications", "Show Notifications")
-    .action(async () => await renderNotifications())
-    .command("d departures", "Show Departures")
-    .arguments("<stationName>")
-    .action(async (_, stationName) => await renderDepartures(stationName))
-    .command("r routes", "Show Routes")
-    .arguments("<fromStation> <toStation>")
-    .action(async (_, fromStation, toStation) =>
-        await renderRoutes(fromStation, toStation)
-    )
+    .command("n notifications", notifications)
+    .command("d departures", departures)
+    .command("r routes", routes)
     .parse(Deno.args);
